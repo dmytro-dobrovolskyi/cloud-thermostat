e5f9097913141f16a149e1a2519470d9c26e2a42
dmytro.dobrovolskyi <dmytro.dobrovolskyi@symphony-solutions.eu>
2018-12-16 04:14:31 +0200

    Add BluetoothManager.getBluetoothManager()

--- src/main/java/com/dmytrodobrovolskyi/couldthermostat/CloudThermostatApplication.java
+++ src/main/java/com/dmytrodobrovolskyi/couldthermostat/CloudThermostatApplication.java
@@ -1,11 +1,13 @@
 package com.dmytrodobrovolskyi.couldthermostat;
 
+import tinyb.BluetoothManager;
+
 /**
  * To debug:
  * sudo java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=y -jar target/cloud-thermostat-0.1.jar
  */
 public class CloudThermostatApplication {
   public static void main(String[] args) {
-
+    BluetoothManager.getBluetoothManager();
   }
 }
