83535f36a5827379f8007a6c44684a949d82064e
dmytro.dobrovolskyi <dmytro.dobrovolskyi@symphony-solutions.eu>
2018-12-16 03:13:41 +0200

    Remove old CloudThermostatApplication code

--- src/main/java/com/dmytrodobrovolskyi/couldthermostat/CloudThermostatApplication.java
+++ src/main/java/com/dmytrodobrovolskyi/couldthermostat/CloudThermostatApplication.java
@@ -5,40 +5,7 @@ package com.dmytrodobrovolskyi.couldthermostat;
  * sudo java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=y -jar target/cloud-thermostat-0.1.jar
  */
 public class CloudThermostatApplication {
-  public static void main(String[] args) throws InterruptedException, BluetoothStateException {
+  public static void main(String[] args) {
 
-    final Object inquiryCompletedEvent = new Object();
-    DiscoveryListener listener = new DiscoveryListener() {
-
-      public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
-        System.out.println("Device " + btDevice.getBluetoothAddress() + " found");
-        try {
-          System.out.println("     name " + btDevice.getFriendlyName(false));
-        } catch (IOException cantGetDeviceName) {
-          throw new RuntimeException(cantGetDeviceName);
-        }
-      }
-
-      public void inquiryCompleted(int discType) {
-        System.out.println("Device Inquiry completed!");
-        synchronized (inquiryCompletedEvent) {
-          inquiryCompletedEvent.notifyAll();
-        }
-      }
-
-      public void serviceSearchCompleted(int transID, int respCode) {
-      }
-
-      public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
-      }
-    };
-
-    synchronized (inquiryCompletedEvent) {
-      boolean started = LocalDevice.getLocalDevice().getDiscoveryAgent().startInquiry(DiscoveryAgent.GIAC, listener);
-      if (started) {
-        System.out.println("wait for device inquiry to complete...");
-        inquiryCompletedEvent.wait();
-      }
-    }
   }
 }
