8076d01b3037761db4a61790d02af4ee51bdce4f
dmytro.dobrovolskyi <dmytro.dobrovolskyi@symphony-solutions.eu>
2019-01-21 03:05:57 +0200

    Add switch off case

--- src/main/java/com/dmytrodobrovolskyi/couldthermostat/service/Thermostat.java
+++ src/main/java/com/dmytrodobrovolskyi/couldthermostat/service/Thermostat.java
@@ -23,22 +23,45 @@ public class Thermostat {
 
     log.info("Current temperature is {}", currentTemperature);
 
-    if (isTooCool(config, currentTemperature) && powerSwitch.isOff()) {
+    if (isHeatingRequired(config, currentTemperature)) {
       powerSwitch.turnOn();
-      log.info("It's too cool but no worries it's gonna be all right");
-    } else if (isTooHot(config, currentTemperature) && powerSwitch.isOff()) {
+      log.info("It's too cool but no worries it's gonna be all right. Turning on the heater!");
+    } else if (isCoolingRequired(config, currentTemperature)) {
       powerSwitch.turnOn();
-      log.info("It's too hot but no worries it's gonna be all right");
+      log.info("It's too hot but no worries it's gonna be all right. Turning on the heater!");
+    } else if (shouldTurnOff(config, currentTemperature)) {
+      powerSwitch.turnOff();
+      log.info("Looks like we did the job well. Turning off the heater/cooler");
     } else {
       log.info("Looks like you're all set!");
     }
   }
 
+  private boolean isCoolingRequired(Config config, double currentTemperature) {
+    return config.isCooling() && isTooHot(config, currentTemperature) && powerSwitch.isOff();
+  }
+
+  private boolean isHeatingRequired(Config config, double currentTemperature) {
+    return config.isHeating() && isTooCool(config, currentTemperature) && powerSwitch.isOff();
+  }
+
+  private boolean shouldTurnOff(Config config, double currentTemperature) {
+    return powerSwitch.isOn() && (isHeatedEnough(config, currentTemperature) || isCooledEnough(config, currentTemperature));
+  }
+
+  private boolean isCooledEnough(Config config, double currentTemperature) {
+    return config.isCooling() && currentTemperature <= config.getTemperature();
+  }
+
+  private boolean isHeatedEnough(Config config, double currentTemperature) {
+    return config.isHeating() && currentTemperature >= config.getTemperature();
+  }
+
   private boolean isTooCool(Config config, double currentTemperature) {
-    return config.isHeating() && currentTemperature < config.getTemperature();
+    return currentTemperature < config.getTemperature();
   }
 
   private boolean isTooHot(Config config, double currentTemperature) {
-    return config.isCooling() && currentTemperature > config.getTemperature();
+    return currentTemperature > config.getTemperature();
   }
 }
