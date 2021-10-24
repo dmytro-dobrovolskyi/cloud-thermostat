7d8b08095bcbee46480e496a8635901a1f083e2d
dmytrodobrovolskyi <dmytro.dobrovolskyi@solarisbank.de>
2021-10-24 07:01:34 +0300

    Tune config

--- src/main/java/com/dmytrodobrovolskyi/couldthermostat/job/TemperatureRegulator.java
+++ src/main/java/com/dmytrodobrovolskyi/couldthermostat/job/TemperatureRegulator.java
@@ -3,12 +3,15 @@ package com.dmytrodobrovolskyi.couldthermostat.job;
 import com.dmytrodobrovolskyi.couldthermostat.service.Thermostat;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class TemperatureRegulator {
     private final Thermostat thermostat;
 
